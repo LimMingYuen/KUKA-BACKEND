cec8a81a54a34e02af6a917b2d347e8178cd5151
Lim Ming Yuen <limmy@qynix.com>
2025-11-15 22:49:13 +0800

    Add External Id

---
+++ QES-KUKA-AMR-API/Migrations/20251115143457_AddExternalId.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QES_KUKA_AMR_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExternalId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExternalWorkflowId",
+                table: "WorkflowDiagrams",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ExternalQrCodeId",
+                table: "QrCodes",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ExternalMobileRobotId",
+                table: "MobileRobots",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ExternalMapZoneId",
+                table: "MapZones",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalWorkflowId",
+                table: "WorkflowDiagrams");
+
+            migrationBuilder.DropColumn(
+                name: "ExternalQrCodeId",
+                table: "QrCodes");
+
+            migrationBuilder.DropColumn(
+                name: "ExternalMobileRobotId",
+                table: "MobileRobots");
+
+            migrationBuilder.DropColumn(
+                name: "ExternalMapZoneId",
+                table: "MapZones");
+        }
+    }
+}
